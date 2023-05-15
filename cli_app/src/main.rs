6fae3696da0c391a9789f8bbd3949ae31e6715f9
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-15 22:34:02 +0530

    Add: (cli_app) Flag arg to struct member.

--- cli_app/src/main.rs
+++ cli_app/src/main.rs
@@ -1,9 +1,14 @@
-use std::{env, path::PathBuf};
 use clap::Parser;
+use std::path::PathBuf;
 
+/// Search a file for a given pattern & display line.
 #[derive(Parser, Debug)]
 struct Cli {
+    /// Pattern to be searched in the file.
+    #[arg(long = "pattern")]
     pattern: String,
+    /// Path of the file to be searched.
+    #[arg(long = "path")]
     path: PathBuf,
 }
 
