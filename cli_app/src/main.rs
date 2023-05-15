6b8d3eeb6c7715242dcefe1ab54d49eed8afb7b6
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-15 23:42:36 +0530

    Refactor: (cli_app) main function return type to result.

--- cli_app/src/main.rs
+++ cli_app/src/main.rs
@@ -1,8 +1,11 @@
 use clap::Parser;
 use std::{
+    error::Error,
+    fmt::Error,
     fs::File,
     io::{BufReader, Read},
     path::PathBuf,
+    process::exit,
 };
 
 /// Search a file for a given pattern & display line.
@@ -16,21 +19,20 @@ struct Cli {
     path: PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read cli args.
     let args = Cli::parse();
 
     // Read file.
-    let file = File::open(&args.path).expect("Unable to open file");
+    let file = File::open(&args.path)?;
     let mut reader = BufReader::new(file);
     // Store file content.
     let mut contents = String::new();
-    let _ = reader
-        .read_to_string(&mut contents)
-        .expect("Unable to read file");
+    let _ = reader.read_to_string(&mut contents)?;
 
     // Print line num & line.
     for (num, line) in contents.lines().enumerate() {
         println!("{:}, {:}", num, line);
     }
+    Ok(())
 }
