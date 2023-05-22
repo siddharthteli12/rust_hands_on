8f854d89543e5d82d58858b6238ec1c8782c1f7c
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-22 18:34:32 +0530

    Add: (cli_app) unit test, find_pattern_in_file.

--- cli_app/tests/cli.rs
+++ cli_app/tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
@@ -10,6 +11,23 @@ fn file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_pattern_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("find_pattern")?;
+    file.write_str("My name is\ndadas\nname")?;
+    // Calling binary with pattern & file path.
+    let mut cmd = Command::cargo_bin("cli_app")?;
+    cmd.arg("--pattern")
+        .arg("name")
+        .arg("--path")
+        .arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("0,My name is\n2,name"));
 
     Ok(())
 }
