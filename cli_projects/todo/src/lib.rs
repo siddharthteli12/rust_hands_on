4dae8b5ee41f688492db99122231d530fc0c33f2
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-15 21:30:26 +0530

    Add: Utility funtion template.

--- cli_projects/todo/src/lib.rs
+++ cli_projects/todo/src/lib.rs
@@ -1,1 +1,9 @@
+pub fn add_task() {}
 
+pub fn delete_task() {}
+
+pub fn complete_task() {}
+
+pub fn edit_task() {}
+
+pub fn list_task() {}
--- cli_projects/todo/src/main.rs
+++ cli_projects/todo/src/main.rs
@@ -1,13 +1,24 @@
 use std::env;
+use todo::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args[1].to_ascii_lowercase().as_str() {
-        "add" => {}
-        "delete" => {}
-        "complete" => {}
-        "edit" => {}
-        "list" => {}
+        "add" => {
+            add_task();
+        }
+        "delete" => {
+            delete_task();
+        }
+        "complete" => {
+            complete_task();
+        }
+        "edit" => {
+            edit_task();
+        }
+        "list" => {
+            list_task();
+        }
         _ => {}
     }
 }
