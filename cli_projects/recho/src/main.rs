cf17cf1abe59bea27c3d1e451e636fa08a87aeb7
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-15 11:46:15 +0530

    Refactor: Optimize with join.

--- cli_projects/recho/src/main.rs
+++ cli_projects/recho/src/main.rs
@@ -2,14 +2,9 @@ use std::env;
 
 fn main() {
     // Read cli args.
-    let mut cli_args: Vec<String> = env::args().collect();
-    // Store user input with whitespace.
-    let mut input_string = String::new();
-    // Remove first index which is name of program.
+    let mut cli_args: Vec<String>= env::args().collect();
+    // Remove first index, which is name of program.
     cli_args.remove(0);
-    // Iterate over args & prefix with whitespace.
-    for string in cli_args {
-        input_string.push_str(&format!(" {:}", string));
-    }
-    println!("{:}", input_string.trim_start());
+    // Join string list with whitespace.
+    println!("{:}", cli_args.join(" "));
 }
