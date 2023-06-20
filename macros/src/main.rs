92da8b2317cfbbe836b8d469097d8ddcae275b4b
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-20 21:34:27 +0530

    Refactor: Combine custom print & vec macro.

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,10 +1,20 @@
-// Simple custom macro impl.
+// Eg 1 custom vec.
+macro_rules! custom_vec {
+    ($($e:expr),*) => {{
+        let mut v = Vec::new();
+        $(v.push($e);)*
+        v
+    }};
+}
+
+// Eg 2 custom print.
 macro_rules! println_wrapper {
-    ($expression:expr) => {
-        println!("{:?}", $expression);
+    ($exp1:expr, $exp2:expr) => {
+        println!("{:?}", $exp2);
     };
 }
 
 fn main() {
-    println_wrapper!("Hello, world!");
+    let vec1 = custom_vec![1, 2, 3];
+    println_wrapper!("{:?}", vec1);
 }
