759cf14caf816da47ccaf5b69a842f90cd88ea55
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-21 22:53:55 +0530

    Add: Struct & box for pointer eg.

--- random1/src/main.rs
+++ random1/src/main.rs
@@ -1,12 +1,40 @@
-unsafe fn modify_pointer(pointer: *mut i32) -> i32 {
-    *pointer = 10 as i32;
-    println!("Pointer value {:?}", pointer);
-    *pointer
+unsafe fn modify_pointer(pointer: *mut Vec<i32>) {
+    *pointer = vec![5, 6, 7, 8];
+}
+
+unsafe fn modify_pointer_box(pointer: *mut Box<i32>) {
+    *pointer = Box::new(10);
+    println!("Box pointer - {:?}", pointer);
+}
+
+unsafe fn modify_pointer_human(pointer: *mut Human) {
+    *pointer = Human {
+        name: "Dumb".to_string(),
+        age: 100,
+    };
+    println!("Human pointer - {:?}", pointer);
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Human {
+    name: String,
+    age: i32,
 }
 
 fn main() {
-    let mut a = 12;
+    let mut a: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let mut b = Box::new(10);
+    let mut human = Human {
+        name: String::from("Sid"),
+        age: 23,
+    };
     unsafe {
-        println!("{:}", modify_pointer(&mut a));
+        modify_pointer(&mut a);
+        modify_pointer_box(&mut b);
+        modify_pointer_human(&mut human);
     }
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", human);
 }
