a32e751b15338686efd8d644f06d6af9f3a4bda2
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-26 17:56:24 +0530

    Refactor: Add expect for result type.

--- cli_projects/todo/src/lib.rs
+++ cli_projects/todo/src/lib.rs
@@ -13,9 +13,7 @@ pub fn add_task(task: String) {
         .open("todo.txt")
         .unwrap();
 
-    if let Err(e) = writeln!(file, "{:}", task) {
-        eprintln!("Issue writing to file due to {:}", e);
-    }
+    writeln!(file, "{:}", task).expect("Issue writing to file");
 }
 
 pub fn delete_task(index: usize) {
@@ -23,12 +21,12 @@ pub fn delete_task(index: usize) {
         .write(true)
         .read(true)
         .append(true)
-        .create(true)
         .open("todo.txt")
         .unwrap();
 
     let mut tasks = String::new();
-    let _ = file.read_to_string(&mut tasks);
+    file.read_to_string(&mut tasks)
+        .expect("Issue reading to string");
     let result_task: Vec<&str> = tasks
         .lines()
         .enumerate()
@@ -37,9 +35,10 @@ pub fn delete_task(index: usize) {
         .map(|(_, e)| e)
         .collect();
 
-    let _ = file.set_len(0);
+    file.set_len(0).expect("Issue setting file len to 0");
     for task in result_task {
-        let _ = file.write(format!("{:}\n", task).as_bytes());
+        file.write(format!("{:}\n", task).as_bytes())
+            .expect("Issue writing to file");
     }
 }
 
@@ -53,7 +52,8 @@ pub fn complete_task(index: usize) {
         .unwrap();
 
     let mut result_tasks = String::new();
-    let _ = file.read_to_string(&mut result_tasks);
+    file.read_to_string(&mut result_tasks)
+        .expect("Issue reading from file");
     let mut tasks: Vec<&str> = result_tasks.lines().collect();
 
     if tasks[index].starts_with("*") {
@@ -61,9 +61,10 @@ pub fn complete_task(index: usize) {
     } else {
         let completed_task = format!("*{:}", tasks[index]);
         tasks[index] = &completed_task;
-        let _ = file.set_len(0);
+        file.set_len(0).expect("Issue setting file len to 0");
         for task in tasks {
-            let _ = file.write(format!("{:}\n", task).as_bytes());
+            file.write(format!("{:}\n", task).as_bytes())
+                .expect("Issue writing to file");
         }
     }
 }
@@ -77,14 +78,16 @@ pub fn edit_task(index: usize, new_task: String) {
         .open("todo.txt")
         .unwrap();
     let mut tasks = String::new();
-    let _ = file.read_to_string(&mut tasks);
+    file.read_to_string(&mut tasks)
+        .expect("Issue reading to string");
 
     let mut tasks: Vec<&str> = tasks.lines().collect();
 
     tasks[index] = &new_task;
-    let _ = file.set_len(0);
+    file.set_len(0).expect("Issue setting file len to 0");
     for task in tasks {
-        let _ = file.write(format!("{:}\n", task).as_bytes());
+        file.write(format!("{:}\n", task).as_bytes())
+            .expect("Issue writing to file");
     }
 }
 
@@ -96,7 +99,8 @@ pub fn list_task() {
         .unwrap();
 
     let mut result_tasks = String::new();
-    let _ = file.read_to_string(&mut result_tasks);
+    file.read_to_string(&mut result_tasks)
+        .expect("Issue reading to string");
 
     for (index, task) in result_tasks.lines().enumerate() {
         if task.chars().nth(0).unwrap() == '*' {
