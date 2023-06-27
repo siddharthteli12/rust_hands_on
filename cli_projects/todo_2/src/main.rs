bb04243da3dc9aef23fe105fce421ea61b2bb064
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-27 22:44:33 +0530

    Add: main file cli args.

--- cli_projects/todo_2/src/main.rs
+++ cli_projects/todo_2/src/main.rs
@@ -1,31 +1,39 @@
-// use std::env;
-// use todo::*;
+use std::env;
+use todo_2::*;
 
-// const MESSAGE: &str = "Incorrect subcommand, only accepted subcommand is-\nadd\ndelete\ncomplete\nedit\nlist";
+const MESSAGE: &str =
+    "Incorrect subcommand, only accepted subcommand is-\nadd\ndelete\ncomplete\nedit\nlist";
+const TODO_PATH: &str = "todo.txt";
 
-// fn main() {
-//     let args: Vec<String> = env::args().collect();
-
-//     match args[1].to_ascii_lowercase().as_str() {
-//         "add" => {
-//             add_task(args[2..].join(" "));
-//         }
-//         "delete" => {
-//             delete_task(args[2].parse::<usize>().unwrap());
-//         }
-//         "complete" => {
-//             complete_task(args[2].parse::<usize>().unwrap());
-//         }
-//         "edit" => {
-//             edit_task(args[2].parse::<usize>().unwrap(), args[3..].join(" "));
-//         }
-//         "list" => {
-//             list_task();
-//         }
-//         _ => {
-//             println!("{MESSAGE}")
-//         }
-//     }
-// }
-
-fn main() {}
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(arg) => {
+            let mut todo_list = TodoList::new(TODO_PATH);
+            match arg.to_ascii_lowercase().as_str() {
+                "add" => {
+                    todo_list.add_todo(args[2..].join(" "));
+                }
+                "delete" => {
+                    todo_list.delete_todo(args[2].parse::<usize>().unwrap());
+                }
+                "complete" => {
+                    todo_list.mark_todo_as_complete(args[2].parse::<usize>().unwrap());
+                }
+                "edit" => {
+                    todo_list.edit_todo(args[2].parse::<usize>().unwrap(), args[3..].join(" "));
+                }
+                "list" => {
+                    todo_list.list_todo();
+                }
+                _ => {
+                    println!("{MESSAGE}")
+                }
+            };
+            todo_list.write_todos_to_file();
+        }
+        None => {
+            println!("Missing cli arguments")
+        }
+    }
+}
