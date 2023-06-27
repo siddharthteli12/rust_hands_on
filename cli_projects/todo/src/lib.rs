c1891dc442c7551f5242c79f9d525c4184512983
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-27 18:06:15 +0530

    Refactor: Remove useless file feature.
    Impl
    -Remove append feature.
    -Add rewind to reset pointer.

--- cli_projects/todo/src/lib.rs
+++ cli_projects/todo/src/lib.rs
@@ -7,7 +7,6 @@ use colored::Colorize;
 
 pub fn add_task(task: String) {
     let mut file = OpenOptions::new()
-        .write(true)
         .append(true)
         .create(true)
         .open("todo.txt")
@@ -27,7 +26,7 @@ pub fn delete_task(index: usize) {
     file.read_to_string(&mut tasks)
         .expect("Issue reading to string");
 
-    let result_task: String = tasks
+    let tasks: String = tasks
         .lines()
         .enumerate()
         .into_iter()
@@ -37,42 +36,37 @@ pub fn delete_task(index: usize) {
 
     file.set_len(0).expect("Issue in settin len to zero");
     file.rewind().expect("Issue rewinding");
-    file.write(result_task.as_bytes())
-        .expect("Issue writing to file");
+    file.write(tasks.as_bytes()).expect("Issue writing to file");
 }
 
 pub fn complete_task(index: usize) {
     let mut file = OpenOptions::new()
-        .write(true)
         .read(true)
-        .append(true)
+        .write(true)
         .open("todo.txt")
         .unwrap();
 
-    let mut result_tasks = String::new();
-    file.read_to_string(&mut result_tasks)
+    let mut tasks = String::new();
+    file.read_to_string(&mut tasks)
         .expect("Issue reading from file");
-    let mut tasks: Vec<&str> = result_tasks.lines().collect();
+    let mut tasks: Vec<&str> = tasks.lines().collect();
 
     if tasks[index].starts_with("*") {
         return;
     } else {
-        let completed_task = format!("*{:}", tasks[index]);
-        tasks[index] = &completed_task;
+        let complete_task = format!("*{:}", tasks[index]);
+        tasks[index] = &complete_task;
         file.set_len(0).expect("Issue setting file len to 0");
-        for task in tasks {
-            file.write(format!("{:}\n", task).as_bytes())
-                .expect("Issue writing to file");
-        }
+        file.rewind().expect("Issue rewinding");
+        file.write((tasks.join("\n") + "\n").as_bytes())
+            .expect("Issue writing to file");
     }
 }
 
 pub fn edit_task(index: usize, new_task: String) {
     let mut file = OpenOptions::new()
-        .write(true)
         .read(true)
-        .append(true)
-        .create(true)
+        .write(true)
         .open("todo.txt")
         .unwrap();
     let mut tasks = String::new();
@@ -83,24 +77,19 @@ pub fn edit_task(index: usize, new_task: String) {
 
     tasks[index] = &new_task;
     file.set_len(0).expect("Issue setting file len to 0");
-    for task in tasks {
-        file.write(format!("{:}\n", task).as_bytes())
-            .expect("Issue writing to file");
-    }
+    file.rewind().expect("Issue rewinding");
+    file.write((tasks.join("\n") + "\n").as_bytes())
+        .expect("Issue writing to file");
 }
 
 pub fn list_task() {
-    let mut file = OpenOptions::new()
-        .read(true)
-        .append(true)
-        .open("todo.txt")
-        .unwrap();
+    let mut file = OpenOptions::new().read(true).open("todo.txt").unwrap();
 
-    let mut result_tasks = String::new();
-    file.read_to_string(&mut result_tasks)
+    let mut tasks = String::new();
+    file.read_to_string(&mut tasks)
         .expect("Issue reading to string");
 
-    for (index, task) in result_tasks.lines().enumerate() {
+    for (index, task) in tasks.lines().enumerate() {
         if task.chars().nth(0).unwrap() == '*' {
             println!(
                 "{:} {:}",
