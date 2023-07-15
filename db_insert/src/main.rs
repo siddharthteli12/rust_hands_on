10fd1774859cb70e3d880c134f6951941ffb56d0
siddharthteli12 <telisiddharth123@yahoo.in>
2023-07-16 01:00:53 +0530

    Fix: Option type issue with query.

--- db_insert/src/main.rs
+++ db_insert/src/main.rs
@@ -1,30 +1,12 @@
 use dotenv::dotenv;
 
-pub struct Student {
-    name: String,
-    age: Option<i32>,
-}
-
-impl Student {
-    fn new(name: String, age: i32) -> Self {
-        Self {
-            name,
-            age: Some(age),
-        }
-    }
-}
 
 #[tokio::main]
 pub async fn main() {
     dotenv().ok();
-    let students = vec![
-        Student::new("Sid".to_string(), 23),
-        Student::new("Akhil".to_string(), 24),
-        Student::new("Tanuj".to_string(), 23),
-    ];
 
     let names = vec!["Sid".to_string(), "Akhil".to_string(), "Tanuj".to_string()];
-    let ages = vec![Some(23), Some(24), Some(25)];
+    let ages: Vec<Option<i64>> = vec![Some(23), None, None];
 
     let url = std::env::var("DATABASE_URL").expect("ENV VARIABLE must be set.");
 
@@ -37,7 +19,7 @@ pub async fn main() {
         "INSERT INTO students(name ,age) 
         SELECT * FROM UNNEST($1::text[], $2::int8[])",
         &names[..],
-        &ages[..]
+        &ages[..]: Vec<Option<i64>>,
     )
     .execute(&pool)
     .await
