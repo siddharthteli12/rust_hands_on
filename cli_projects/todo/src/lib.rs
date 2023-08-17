1421e31d5abf2f49f17debff5210359f147c30e8
siddharthteli12 <telisiddharth123@yahoo.in>
2023-08-17 10:22:59 +0530

    Optimize code.

--- cli_projects/todo/src/lib.rs
+++ cli_projects/todo/src/lib.rs
@@ -29,14 +29,14 @@ pub fn delete_task(index: usize) {
     let tasks: String = tasks
         .lines()
         .enumerate()
-        .into_iter()
         .filter(|&(i, _)| i != index)
         .map(|(_, e)| format!("{:}\n", e))
         .collect();
 
     file.set_len(0).expect("Issue in settin len to zero");
     file.rewind().expect("Issue rewinding");
-    file.write(tasks.as_bytes()).expect("Issue writing to file");
+    file.write_all(tasks.as_bytes())
+        .expect("Issue writing to file");
 }
 
 pub fn complete_task(index: usize) {
@@ -51,14 +51,12 @@ pub fn complete_task(index: usize) {
         .expect("Issue reading from file");
     let mut tasks: Vec<&str> = tasks.lines().collect();
 
-    if tasks[index].starts_with("*") {
-        return;
-    } else {
+    if !tasks[index].starts_with('*') {
         let complete_task = format!("*{:}", tasks[index]);
         tasks[index] = &complete_task;
         file.set_len(0).expect("Issue setting file len to 0");
         file.rewind().expect("Issue rewinding");
-        file.write((tasks.join("\n") + "\n").as_bytes())
+        file.write_all((tasks.join("\n") + "\n").as_bytes())
             .expect("Issue writing to file");
     }
 }
@@ -78,7 +76,7 @@ pub fn edit_task(index: usize, new_task: String) {
     tasks[index] = &new_task;
     file.set_len(0).expect("Issue setting file len to 0");
     file.rewind().expect("Issue rewinding");
-    file.write((tasks.join("\n") + "\n").as_bytes())
+    file.write_all((tasks.join("\n") + "\n").as_bytes())
         .expect("Issue writing to file");
 }
 
@@ -90,11 +88,11 @@ pub fn list_task() {
         .expect("Issue reading to string");
 
     for (index, task) in tasks.lines().enumerate() {
-        if task.chars().nth(0).unwrap() == '*' {
+        if let Some(task_str) = task.strip_prefix('*') {
             println!(
                 "{:} {:}",
                 index.to_string().bold(),
-                task[1..].strikethrough()
+                task_str.strikethrough()
             );
         } else {
             println!("{:} {:}", index.to_string().bold(), task.green());
