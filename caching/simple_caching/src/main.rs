e7c546a389a24b8bc4b73b33adf4834460c186bb
siddharthteli12 <telisiddharth123@yahoo.in>
2023-12-12 17:04:41 +0530

    Refactor: Insert to cache when key not found

--- caching/simple_caching/src/main.rs
+++ caching/simple_caching/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io};
+use std::{cell::Cell, collections::HashMap, io, ops::Deref};
 
 // DbPool connection
 struct DbPool;
@@ -6,30 +6,31 @@ struct DbPool;
 impl DbPool {
     pub fn process_request_id(&self, id: u32) -> io::Result<Response> {
         // Very expensive db operation.
-        Ok(Response {
-            value: "Hello".to_string(),
-        })
+        Ok(Response { value: 10000000 })
     }
 }
 struct AppState {
     db: DbPool,
-    cache: HashMap<u32, Response>,
+    cache: Cell<HashMap<u32, Response>>,
 }
 
 // Response
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Response {
-    value: String,
+    value: u128,
 }
 
 impl AppState {
     fn get_response(&self, id: u32) -> io::Result<Response> {
         // Return from cache or else from db.
-        if let Some(value) = self.cache.get(&id) {
-            return Ok(value.clone());
-        } else {
-            self.db.process_request_id(id)
+        if let Some(value) = self.cache.take().get(&id) {
+            return Ok(*value);
         }
+        let value = self.db.process_request_id(id);
+        let mut map = self.cache.take();
+        map.insert(id, *value.as_ref().unwrap());
+        self.cache.set(map);
+        value
     }
 }
 
