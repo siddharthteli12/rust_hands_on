310af7886e4d6614bd8e8f47c9bde9e824e71666
siddharthteli12 <telisiddharth123@yahoo.in>
2024-03-31 11:59:29 +0530

    Add: Normal vs simd operation

--- simd/README.md
+++ simd/README.md
@@ -3,3 +3,7 @@
     - It can make processing 4 times faster depending on num of data we are processing.
     - There's limited context where simd operations can we used.
     - Simd is cpu specific too, Mostly all cpu support simd instruction but have some varience.
+
+-  Running code
+    - `cargo +nightly run`
+    - The simd addition is twice has fast with normal addition.
--- simd/src/main.rs
+++ simd/src/main.rs
@@ -1,26 +1,36 @@
 #![feature(portable_simd)]
-use std::simd::*;
-use std::time::Instant;
-use std::time::Duration;
-use std::thread::sleep;
+
+use packed_simd::*;
+
+const LANES: usize = 16;
+const LENGTH: usize = LANES * 50000;
 
 fn main() {
-    let mut data1 = [43; 100000]; 
-    let mut data2 = [53; 100000];
-    let now = Instant::now();
-    normal_addition(&mut data1, &mut data2);
-    let time1 = now.elapsed().as_secs();
-    println!("Normal addition time - {time1}");
-    simd_addition(&mut data1, &mut data2);
-    println!("Simd addition time - {}", now.elapsed().as_secs() - time1);
+    let data1 = [43; LENGTH];
+    let data2 = [53; LENGTH];
+    let now = std::time::Instant::now();
+    normal_addition(&data1, &data2);
+    let time1 = now.elapsed().as_millis();
+    println!("Normal addition time - {}", time1);
+    simd_addition(&data1, &data2);
+    println!("Simd addition time - {}", now.elapsed().as_millis() - time1);
 }
 
-
-fn normal_addition(data1: &mut [i32], data2: &mut [i32]) {
-    sleep(Duration::new(2, 0));
-
+fn normal_addition(data1: &[i32], data2: &[i32]) {
+    let mut result = vec![0; data1.len()];
+    for i in 0..data1.len() {
+        result[i] = data1[i] + data2[i];
+    }
 }
 
-fn simd_addition(data1: &mut [i32], data2: &mut [i32]) {
-    sleep(Duration::new(1, 0));
+fn simd_addition(data1: &[i32], data2: &[i32]) {
+    assert_eq!(data1.len(), data2.len(), "Array lengths must be equal");
+    let mut result = vec![0; data1.len()];
+    let simd_width = i32x16::lanes();
+    for i in (0..data1.len()).step_by(simd_width) {
+        let a_chunk = i32x16::from_slice_unaligned(&data1[i..]);
+        let b_chunk = i32x16::from_slice_unaligned(&data2[i..]);
+        let sum = a_chunk + b_chunk;
+        sum.write_to_slice_unaligned(&mut result[i..]);
+    }
 }
