54a02bdfd0badc4955ad6c5964a1c094c3972e13
siddharthteli12 <telisiddharth123@yahoo.in>
2024-04-01 23:22:32 +0530

    Refactor: Minor optimize

--- simd_json/src/main.rs
+++ simd_json/src/main.rs
@@ -7,24 +7,25 @@ const LANES: usize = 16;
 const BUFFER_SIZE: usize = 512 * 16;
 
 fn main() -> std::io::Result<()> {
-    let input_file = File::open("input.json")?;
-    let output_file = File::create("output.json")?;
     let now = Instant::now();
-    process_json(input_file, output_file);
+    process_json("input.json", "output.json")?;
     println!("Time taken - {:?}", now.elapsed().as_secs());
     Ok(())
 }
 
-fn process_json(mut input: File, mut output: File) {
+fn process_json(input_path: &str, output_path: &str) -> std::io::Result<()> {
+    let mut input_file = File::open(input_path)?;
+    let mut output_file = File::create(output_path)?;
     let mut buffer = [0_u8; BUFFER_SIZE];
     loop {
-        let read_bytes = input.read(&mut buffer).unwrap();
+        let read_bytes = input_file.read(&mut buffer).unwrap();
         if !read_bytes % LANES == 0 {
             break;
         }
         let result = process_json_chunks(&buffer[..read_bytes]);
-        output.write(&result).unwrap();
+        output_file.write(&result).unwrap();
     }
+    Ok(())
 }
 
 fn process_json_chunks(list: &[u8]) -> Vec<u8> {
@@ -32,9 +33,9 @@ fn process_json_chunks(list: &[u8]) -> Vec<u8> {
 
     for i in (0..list.len()).step_by(LANES) {
         let a = u8x16::from_slice_unaligned(&list[i..]);
-        let b = u8x16::splat(';' as u8);
+        let b = u8x16::splat(b';');
         let mask = a.eq(b);
-        let replaced = mask.select(u8x16::splat(':' as u8), a);
+        let replaced = mask.select(u8x16::splat(b':'), a);
         replaced.write_to_slice_unaligned(&mut result[i..]);
     }
     result
